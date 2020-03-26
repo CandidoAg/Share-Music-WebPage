6498811e38431ea36d953357b91ecbc4a698cc8d
candido.aguilar <candido.aguilar@vipera.com>
2020-03-26 17:26:21 +0100

    Login done

--- shareMusic/views.py
+++ shareMusic/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.models import User
 
-##import getDataAPI
+import getDataAPI
 
 ##Valores státicos para pruebas
 artista = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+
+# TODO Branch User
+#  access to pages if logged
+#  logout(si no lo hace)
+#  change getData to use the user get in login
+
+
+def login(request):
+    if User:
+        return render(request, "shareMusic/login.html")
+    else:
+        return home(request)
+
 
 # Create your views here.
 def home(request):
@@ -23,44 +36,44 @@ def playlist(request):
     return render(request, 'shareMusic/Playlists.html', context)
 
 
-# def descubrimiento(request):
-#     categories = sorted(getDataAPI.getAllCategories(), reverse=False, key=lambda x: x['name'])
-#     context = {
-#         'categories': categories,
-#     }
-#     return render(request, 'shareMusic/Discover.html', context)
-#
-#
-# def categoryDetails(request, categoryId): #TODO Salen categorias a veces que no existe el id
-#     categoryName,data = getDataAPI.getPlayListByCategory(categoryId)
-#     dataItems = data['playlists']['items']
-#     playLists = []
-#     for playList in dataItems:
-#         playLists.append({'name': playList['name'], 'id': playList['id'], 'like': False})
-#
-#     context = {
-#         'categoryName': categoryName,
-#         'playLists': playLists,
-#     }
-#     return render(request, 'shareMusic/PlayListByCategory.html', context)
-#
-#
-# def songByCategoryPlayList(request, playListId):
-#     playListName, data = getDataAPI.getSongsByPlayList(playListId)
-#     items = data['items']
-#     songs = []
-#     for item in items:
-#             artists = ""
-#             for artist in item['track']['artists']:
-#                 artists += artist['name']+", "
-#             artists = artists[:-2]
-#             songs.append({'name': item['track']['name'], 'artists': artists, 'like': False})
-#
-#     context = {
-#         'categoryName': playListName,  #TODO put name of playList
-#         'songs': songs,
-#     }
-#     return render(request, 'shareMusic/songsByCategoryList.html', context)
+def descubrimiento(request):
+    categories = sorted(getDataAPI.getAllCategories(), reverse=False, key=lambda x: x['name'])
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'shareMusic/Discover.html', context)
+
+
+def categoryDetails(request, categoryId): #TODO Salen categorias a veces que no existe el id
+    categoryName,data = getDataAPI.getPlayListByCategory(categoryId)
+    dataItems = data['playlists']['items']
+    playLists = []
+    for playList in dataItems:
+        playLists.append({'name': playList['name'], 'id': playList['id'], 'like': False})
+
+    context = {
+        'categoryName': categoryName,
+        'playLists': playLists,
+    }
+    return render(request, 'shareMusic/PlayListByCategory.html', context)
+
+
+def songByCategoryPlayList(request, playListId):
+    playListName, data = getDataAPI.getSongsByPlayList(playListId)
+    items = data['items']
+    songs = []
+    for item in items:
+            artists = ""
+            for artist in item['track']['artists']:
+                artists += artist['name']+", "
+            artists = artists[:-2]
+            songs.append({'name': item['track']['name'], 'artists': artists, 'like': False})
+
+    context = {
+        'categoryName': playListName,
+        'songs': songs,
+    }
+    return render(request, 'shareMusic/songsByCategoryList.html', context)
 
 
 def perfil(request):
@@ -70,7 +83,3 @@ def perfil(request):
         'user': user
     }
     return render(request, 'shareMusic/Perfil.html', context)
-
-
-def logout(request):
-    return render(request, 'shareMusic/Logout.html')
